c833418f7b040c54a2aaf0c2621993b69b48f38f
reka <debreka@gmail.com>
2023-02-08 16:03:55 +0100

    show marker properly

--- olithomework_1_google_maps/src/App.js
+++ olithomework_1_google_maps/src/App.js
@@ -1,18 +1,26 @@
 
 import './App.css';
-import { GoogleMap, LoadScript } from '@react-google-maps/api';
+import { GoogleMap, LoadScript, MarkerF } from '@react-google-maps/api';
+import { useState } from 'react';
+
+//https://react-google-maps-api-docs.netlify
 
 
 
 const containerStyle = {
-  width: '400px',
-  height: '400px'
+  width: '600px',
+  height: '600px'
 };
 
-const center = {
-  lat: -3.745,
-  lng: -38.523
+const dabasCoord = {
+  lat: 47.18694,
+  lng: 19.31091
 };
+
+const onLiveITCoord = {
+  lat : 47.1916,
+  lng : 19.32848
+}
 
 const APIKEY = process.env.REACT_APP_GOOGLEMAPS_API_KEY
 const functioningAPIKEY= 'AIzaSyBpPI_5Q2VofDHHrePZzg0oNe4D2y_aGKE'
@@ -20,10 +28,18 @@ const functioningAPIKEY= 'AIzaSyBpPI_5Q2VofDHHrePZzg0oNe4D2y_aGKE'
 
 function App() {
 
+  
+  const [center, setcenter] = useState(dabasCoord);
+  const [marker, setMarker] = useState(onLiveITCoord);
+
+  console.log(marker)
+/* 
   console.log(APIKEY)
-  console.log(functioningAPIKEY)
+  console.log(functioningAPIKEY) */
  
   return (
+ 
+
     <LoadScript
      // googleMapsApiKey={'AIzaSyBpPI_5Q2VofDHHrePZzg0oNe4D2y_aGKE'} 
       googleMapsApiKey={functioningAPIKEY} 
@@ -32,10 +48,10 @@ function App() {
       <GoogleMap
         mapContainerStyle={containerStyle}
         center={center}
-        zoom={10}
+        zoom={9}
       >
-        { /* Child components, such as markers, info windows, etc. */ }
-        <></>
+       
+        <MarkerF position={marker} />
       </GoogleMap>
     </LoadScript>
   );
